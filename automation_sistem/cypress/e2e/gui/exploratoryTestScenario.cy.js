180650dbb5ef2479b9b6f74ff1482ae2908aeaa9
Igor Duarte Alves <yuri@hcmsolucoes.com.br>
2024-01-22 13:14:52 -0300

    Code Structure

---
+++ automation_sistem/cypress/e2e/gui/exploratoryTestScenario.cy.js
@@ -0,0 +1,18 @@
+/// <reference types ="cypress"/>
+
+
+describe('Exploratory Test Scenario.', { tags: ['exploratory', 'smoke', 'parceiros'] }, () => {
+   
+  
+    describe('As a User.', () => {
+      it('Navigate the platform.', () => {
+
+          cy.visit('https://google.com.br');
+      
+      
+      });
+
+  
+    });
+
+});
